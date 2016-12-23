303769b3fae6bcb531d9e87bb3819cdae58ff551
Eeo Jun <141bytes@gmail.com>
2016-12-23 10:06:31 +0800

    Slim down output size

--- Makefile
+++ Makefile
@@ -1,4 +1,14 @@
-deploy:
-	mkdir -p         ~/code/website/rwatch
-	cp ./rwatch.html ~/code/website/rwatch/index.html
-	cp -r ./ext      ~/code/website/rwatch
+debug:
+	mkdir -p build
+	cat ext/*.js src/*.js > build/out.js
+	cat css/*.css         > build/out.css
+
+release: debug
+	cat build/out.js | uglifyjs -mc > build/out.min.js
+	mv build/out.min.js build/out.js
+	minify --output build/out.css css/*.css
+
+deploy: release
+	mkdir -p        ~/code/website/rwatch
+	cp ./index.html ~/code/website/rwatch/index.html
+	cp -r ./build   ~/code/website/rwatch
---
+++ src/app.js
@@ -0,0 +1,90 @@
+var $ = nut;
+var pusher = new Pusher('50ed18dd967b455393ed');
+var message = $.el('#message');
+var input = $.el('#subreddit');
+var subs = $.el('#subreddits');
+var subscriptions = store.get('subs') || [];
+
+function makeListing(listing) {
+  return kr.div({class: 'post'}, [
+    kr.a({target: '_blank', href: 'https://www.reddit.com' + listing.permalink}, kr.h3(listing.title)),
+    kr.span({class: 'time', 'data-time': listing.created_utc}, vagueTime.get({
+      to: listing.created_utc,
+      units: 's',
+    })),
+  ]);
+}
+
+function makeSubreddit(sub) {
+  var unsubscribe = kr.button({class: 'unsubscribe'}, 'X');
+  var posts = kr.div({class: 'posts'});
+  var div = kr.div({class: 'subreddit'}, [
+    kr.div({class: 'header'}, [unsubscribe, kr.h2(sub)]),
+    posts,
+  ]);
+  var channel = pusher.subscribe(sub);
+  evee.on(unsubscribe, 'click', function(ev) {
+    try {
+      pusher.unsubscribe(sub);
+    } catch (err) {} // pusher throws error if channel name is invalid
+    div.parentNode.removeChild(div);
+    subscriptions.splice(subscriptions.indexOf(sub), 1);
+    store.set('subs', subscriptions);
+  });
+  channel.bind('new-listing', function(listing) {
+    var node = makeListing(listing);
+    posts.insertBefore(node, posts.firstChild);
+  });
+  return div;
+}
+
+function subscribe(sub) {
+  subs.appendChild(makeSubreddit(sub));
+}
+
+var message_map = {
+  'initialized':  { color: 'yellow', text: 'Setting up the connection...' },
+  'connecting':   { color: 'yellow', text: 'Trying to connect, hold on...' },
+  'connected':    { color: 'green', text: "We're good to go!" },
+  'unavailable':  { color: 'red', text: 'You may need to check your connection.' },
+  'failed':       { color: 'red', text: 'Pusher is not supported on this browser.' },
+  'disconnected': { color: 'red', text: "Yikes. Try reloading the page." },
+};
+
+setInterval(function() {
+  $('.post').forEach(function(el)  {
+    var span = $.el('.time', el);
+    span.textContent = vagueTime.get({
+      to: +span.getAttribute('data-time'),
+      units: 's',
+    });
+  });
+}, 10*1000);
+
+pusher.connection.bind('state_change', function(states) {
+  message.innerHTML = '';
+  var info = message_map[states.current];
+  message.classList.add(info.color);
+  message.appendChild(kr.p(info.text));
+  setTimeout(function() {
+    message.innerHTML = '';
+    message.classList.remove(info.color);
+  }, 2500);
+});
+
+evee.delegate(document.body, 'click', '.post', function(el, post) {
+  post.parentNode.removeChild(post);
+});
+
+evee.on(input, 'keydown', function(ev) {
+  if (ev.which == 10 || ev.which == 13) { // Enter/Return
+    var sub = input.value.toLowerCase();
+    if (subscriptions.indexOf(sub) === -1) {
+      subscriptions.push(sub);
+      store.set('subs', subscriptions);
+      subscribe(sub);
+    }
+  }
+});
+
+subscriptions.forEach(subscribe);
